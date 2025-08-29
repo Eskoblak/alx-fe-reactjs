b462968d50a5cbe9060963141b4ad0fa56b0f1f7
Peter Njuguna <peternjuguna957@gmail.com>
2025-08-29 14:45:30 +0300

    Fix: Add App.jsx

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import React from "react";
+import TodoList from "./components/TodoList";
+
+export default function App() {
+  return (
+    <div>
+      <h1>My Todo App</h1>
+      <TodoList />
+    </div>
+  );
+}
